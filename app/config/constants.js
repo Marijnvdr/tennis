f67892ab2d801dc8f4e2ef605c65b8a6f20ca50c
mvdr <m.ruijt@niposoftware.com>
2017-01-15 16:51:57 +0100

    Added drop down box for selecting tournament

---
+++ app/config/constants.js
@@ -0,0 +1,7 @@
+export default {
+  AllGrandslams: 'All Grand Slams',
+  AusOpen: 'Australian Open',
+  RolandGarros: 'Roland Garros',
+  Wimbledon: 'Wimbledon',
+  UsOpen: 'US Open'
+}
--- app/pods/player/route.js
+++ app/pods/player/route.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+import Constants from 'tennis/config/constants';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('player');
+    return Ember.RSVP.hash({
+      players: this.store.findAll('player'),
+      tournaments: [Constants.AllGrandslams, Constants.AusOpen, Constants.RolandGarros, Constants.Wimbledon, Constants.UsOpen]
+    });
   }
 });
