d4c6dab193250fa17bf320813d9f07672caf48cb
mvdr <m.ruijt@niposoftware.com>
2017-03-01 19:35:28 +0100

    Use classBindings for color

--- app/pods/components/tournament-match/component.js
+++ app/pods/components/tournament-match/component.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
+import Constants from 'tennis/config/constants';
 
 export default Ember.Component.extend({
   attrs: {
-    match: null
-  }
+    match: null,
+    tournament: null
+  },
+  classNames: ['match-container'],
+  classNameBindings: ['matchColor'],
+  matchColor: Ember.computed('tournament', function() {
+    if (this.tournament == Constants.RolandGarros) {
+      return 'color-red';
+    } else {
+      return 'color-green';
+    }
+  })
 });
