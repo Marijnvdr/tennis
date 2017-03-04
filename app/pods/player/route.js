1eafda7d08b7ea6c06864c8e33bb30216ce4b76f
mvdr <m.ruijt@niposoftware.com>
2017-03-04 20:32:55 +0100

    link implemented

--- app/pods/player/route.js
+++ app/pods/player/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import Constants from 'tennis/config/constants';
 
 export default Ember.Route.extend({
-  model: function() {
+  model: function(params) {
     let periodItems = [];
     let currentYear = new Date().getFullYear();
     periodItems.push('Best results');
@@ -11,8 +11,14 @@ export default Ember.Route.extend({
       periodItems.push(i);
     }
 
+    let selectedPlayer = null;
+    if (params.player) {
+      selectedPlayer = this.store.findRecord('player', params.player);
+    }
+
     return Ember.RSVP.hash({
       players: this.store.findAll('player'),
+      selectedPlayer:  selectedPlayer,
       tournaments: [Constants.AllGrandslams, Constants.AusOpen, Constants.RolandGarros, Constants.Wimbledon, Constants.UsOpen],
       period: periodItems
     });
