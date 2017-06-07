39759a5e5332b018dce2bab5dab3b7f8c664bfd5
mvdr <m.ruijt@niposoftware.com>
2017-06-07 11:32:10 +0200

    Even simpler helpers

--- app/helpers/match-result-state-class.js
+++ app/helpers/match-result-state-class.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Helper.extend({
-  compute([match], namedArgs) {
-    let winner = match.get('winner');
-    if (namedArgs.player == winner) {
-      return 'is-winner';
-    } else {
-      return 'has-lost';
-    }
+function matchResult([match], namedArgs) {
+  let winner = match.get('winner');
+  if (namedArgs.player == winner) {
+    return 'is-winner';
+  } else {
+    return 'has-lost';
   }
-});
+}
+
+export default Ember.Helper.helper(matchResult);
+
--- app/helpers/set-tiebreak.js
+++ app/helpers/set-tiebreak.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 
-export default Ember.Helper.extend({
-  compute([match], namedArgs) {
-    let winner = match.get('winner');
-    let tiebreakLost = match.get(`set${namedArgs.set}Tiebreak`);
-    let tiebreakWon = 7;
-    if (tiebreakLost) {
-      if (tiebreakLost > 5) {
-        tiebreakWon = tiebreakLost + 2;
-      }
-    } else {
-      tiebreakWon = '';
+function setTiebreak([match], namedArgs) {
+  let winner = match.get('winner');
+  let tiebreakLost = match.get(`set${namedArgs.set}Tiebreak`);
+  let tiebreakWon = 7;
+  if (tiebreakLost) {
+    if (tiebreakLost > 5) {
+      tiebreakWon = tiebreakLost + 2;
     }
-    if (namedArgs.player == winner) {
-      return tiebreakWon;
-    } else {
-      return tiebreakLost;
-    }
+  } else {
+    tiebreakWon = '';
   }
-});
+  if (namedArgs.player == winner) {
+    return tiebreakWon;
+  } else {
+    return tiebreakLost;
+  }
+}
+
+export default Ember.Helper.helper(setTiebreak);
